7f0c9d2b9c857b3184dbd62ea2f0279d4851f00e
JurayevOgabek04 <jurayevogabek0304>
2022-04-18 15:26:44 +0500

    day 11

--- Script/day10.js
+++ Script/day10.js
@@ -1,19 +1,19 @@
 //  // 1-vazifa
 
-var numbers = [1,3,5,9]
+// var numbers = [1,3,5,9]
 
-function numbersSum(numbers){
-    let summa = 0;
-    for (let i in numbers){
-        summa += +numbers[i];
-    }
+// function numbersSum(numbers){
+//     let summa = 0;
+//     for (let i in numbers){
+//         summa += +numbers[i];
+//     }
 
-    return summa
-}
+//     return summa
+// }
 
-let result = numbersSum(numbers)
+// let result = numbersSum(numbers)
 
-console.log(result);
+// console.log(result);
 
 
 //  // 2-vazifa
@@ -46,7 +46,6 @@ console.log(result);
 
 // var numbers = [3.02,-3.65,5,-9]
 // var numbers1 = []
-// var numbers2 = []
 // function normalizeNumbers(numbers){
 //     for (let i in numbers){
 
@@ -63,3 +62,46 @@ console.log(result);
 // let result = normalizeNumbers(numbers)
 
 // console.log(result);
+
+
+// const list = [['anor',3],["olma", 5],["pamidor",8],["o'rik",10]]
+
+// for (let i = 0; i < list.length ; i++){
+//     for (let j = 0; j < list[i].length; j++){
+//         console.log(list[i][j]);
+//     }
+// }
+   
+
+
+
+
+
+
+
+// let num = [1,2,3,4,5,6,7,8,9]
+
+// let num2 = []
+
+// while(num.length > 0){
+//     let num3 = num.pop()
+//     num2.push(num3)
+//     console.log(num3);
+// }
+
+// console.log(num2);
+
+
+const list3 = [1,2,3,4,5,6,7,8,9,10]
+for (let i in list3){
+    if (list3[i] % 2 === 0){
+        let index = list3.indexOf(list3[i])
+        console.log(index + "--");
+        let n = list3.splice(index,1)[0]
+        console.log(n);
+        list3.push(n)
+
+    } 
+}
+
+console.log(list3);
---
+++ day11.js
@@ -0,0 +1,80 @@
+//  //                            uy ishi - 4
+
+const arr = [5, 2, 1, -10, 8];
+
+arr.sort(function(a,b){return(b-a)})
+
+console.log(arr);
+
+//  // //                   uy ishi -5
+
+// let num = prompt("Son kiriting!");
+
+// const newlist = [];
+// let summa = 0;
+// while (true) {
+//   if (!isNaN(num)) {
+//     newlist.push(num);
+//     num = prompt("Yana son kiriting");
+//   } else {
+//     break;
+//   }
+// }
+
+// function sum(newlist) {
+//   for (let i in newlist) {
+//     summa += +newlist[i];
+//   }
+
+//   return summa;
+// }
+
+// let result = sum(newlist);
+
+// console.log(result);
+
+//  //                     uy ishi -2
+
+// summa = 0
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+// function yigindi(salaries){
+
+//     for (let i in salaries){
+//         summa += salaries[i]
+//     }
+
+//     return summa
+// }
+
+// let result = yigindi(salaries)
+
+// console.log(result);
+
+
+//  //  uy ishi -3
+
+// const newmenu = [];
+// let menu = {
+//   width: 200,
+//   height: 300,
+//   title: "My menu",
+// };
+// function NewMenyu(menu) {
+//   for (let i in menu) {
+//     if (!isNaN(menu[i])){
+//         newmenu[i] = menu[i]*2
+//     } else {
+//         newmenu[i] = menu[i]
+//     }
+//   }
+
+//   return newmenu;
+// }
+
+// let result = NewMenyu(menu);
+
+// console.log(result);
